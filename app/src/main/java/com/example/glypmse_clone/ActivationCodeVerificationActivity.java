d827629b1b52892810778e1b6e756a3b2a197f96
Muhammad Umair Tahir <nobelhighschool@hotmail.com>
2019-07-13 21:39:03 +0500

    Activation code verification layout made

---
+++ app/src/main/java/com/example/glypmse_clone/ActivationCodeVerificationActivity.java
@@ -0,0 +1,14 @@
+package com.example.glypmse_clone;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivationCodeVerificationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTheme(R.style.AppTheme_NoActionBar);
+        setContentView(R.layout.activity_activation_code_verification);
+    }
+}
