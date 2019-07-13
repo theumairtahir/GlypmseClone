0f8866ab3c5e2cfd7e1dd0f2208810c301581339
Muhammad Umair Tahir <nobelhighschool@hotmail.com>
2019-07-13 17:22:36 +0500

    Splash Screen Created

---
+++ app/src/main/java/com/example/glypmse_clone/SplashScreenActivity.java
@@ -0,0 +1,42 @@
+package com.example.glypmse_clone;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreenActivity extends AppCompatActivity {
+    private Handler mWaitHandler = new Handler();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTheme(R.style.AppTheme_NoActionBar);
+        setContentView(R.layout.activity_splash_screen);
+        mWaitHandler.postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+
+                //The following code will execute after the 5 seconds.
+
+                try {
+
+                    //Go to next page i.e, start the next activity.
+                    Intent intent = new Intent(SplashScreenActivity.this, RegisterActivity.class);
+                    startActivity(intent);
+
+                    //Let's Finish Splash Activity since we don't want to show this when user press back button.
+                    finish();
+                } catch (Exception ignored) {
+                    ignored.printStackTrace();
+                }
+            }
+        }, 5000);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mWaitHandler.removeCallbacksAndMessages(null);
+    }
+}
